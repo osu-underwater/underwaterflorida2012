c3f586f19ddba3a096be0d1b89ec37404cfb5b7e
Soo-Hyun Yoo <yoos117@gmail.com>
2012-02-19 22:12:32 -0800

    Flesh out main loop structure.

--- micro/micro.ino
+++ micro/micro.ino
@@ -3,19 +3,53 @@
  *  \brief ROV main controller loop.
  */
 
+#include <Wire.h>
 #include "ethcomm.cpp"
+#include "imu.cpp"
 
 int main(void) {
     init();   // For Arduino.
+
+    // Begin Arduino services.
+    Wire.begin();
 
     Serial.begin(115200);
 
     //EthComm eth;
 
+    IMU imu;
+    imu.init();
+
     Serial.println("Setup complete.");
 
+    uint64_t nextRunTime = micros();
+    loopCount = 0;
+
     while (1) {
-        //eth.test();
+        if (micros() >= nextRunTime) {
+            // ================================================================
+            // System loop
+            // ================================================================
+            imu.update();   // Run this ASAP when loop starts so gyro integration is as accurate as possible.
+            nextRunTime += MASTER_DT;   // Update next loop start time.
+
+            // ================================================================
+            // Control loop
+            // ================================================================
+            if (loopCount % CONTROL_LOOP_INTERVAL == 0) {
+                // UPDATE PINS HERE
+            }
+
+            // ================================================================
+            // Telemetry loop
+            // ================================================================
+            if (loopCount % TELEMETRY_LOOP_INTERVAL == 0) {
+                //eth.test();
+            }
+
+            loopCount++;
+            loopCount = loopCount % 1000;
+        }
     }
 
     return 0;
